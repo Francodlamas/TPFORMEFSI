f422ea3293eef84da807268c1e3277001caee921
FRANCO DAMIAN <46647617@ort>
2022-05-16 10:36:21 -0300

    first commit

---
+++ Controller.js
@@ -0,0 +1,12 @@
+var formularioPrincipal = document.getElementById("formulario");
+
+var formularioPrincipal = document.getElementById("formulario");
+
+
+function obtenerValores() {
+
+    elemento1.innerHTML=document.getElementById("Matematica").value;
+    elemento2.innerHTML=document.getElementById("Lengua").value;
+    elemento3.innerHTML=document.getElementById("EFSI").value;
+
+};
