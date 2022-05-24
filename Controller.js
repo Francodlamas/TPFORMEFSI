17a6bd359945f44651480cc5bf0be14076106667
FRANCO DAMIAN <46647617@ort>
2022-05-24 14:24:07 -0300

    Rowen

--- Controller.js
+++ Controller.js
@@ -3,9 +3,9 @@
     let elemento2=parseInt(document.getElementById("Lengua").value);
     let elemento3=parseInt(document.getElementById("EFSI").value);
 
-    let aux=false;
+    let aux=true;
         if(elemento1 ==null || elemento1==''|| isNaN(elemento1) || elemento2 ==null || elemento2=='' || isNaN(elemento2) || elemento3 ==null || elemento3=='' || isNaN(elemento3)){
-            aux=true;
+            aux=false;
         }
     if(!aux){
         alert("Falta completar!")
@@ -29,15 +29,25 @@ function promedio() {
     if(aux==true){
     let elemento1=parseInt(document.getElementById("Matematica").value);
     let elemento2=parseInt(document.getElementById("Lengua").value);
-    let elemento3=parseInt(document.getElementById("EFSI").value);
-    let promedio=(elemento3+elemento2+elemento1)/3;
+    let elemento3=parseInt(document.getElementById("EFSI").value); 
+    let promedio=(elemento3+elemento2+elemento1)/3;   
+    if(elemento1 > 10 || elemento1 < 1){
+        alert('Error, usted debe colocar en matemática un valor mayor a 1 y menor que 10');
+    }
+    else if(elemento2 > 10 || elemento2 < 1){
+        alert('Error, usted debe colocar en lengua un valor mayor a 1 y menor que 10');
+    }
+    else if(elemento3 > 10 || elemento3 < 1){
+        alert('Error, usted debe colocar en EFSI un valor mayor a 1 y menor que 10');
+    }else{    document.getElementById("Promedio").innerHTML=promedio;}
 
     if(promedio>=6 && promedio <=10){
-        document.getElementById(promedio).style.color="green";
+     
+        document.getElementById("Promedio").style.color="green";
     }else{
-        document.getElementById(promedio).style.color="red";
+        document.getElementById("Promedio").style.color="red";
     }
-    document.getElementById("Promedio").innerHTML=promedio;
+
 }
 }
 
@@ -51,25 +61,29 @@ function MayorNota() {
     let elemento3=parseInt(document.getElementById("EFSI").value);
 
     if(elemento1>elemento2 && elemento1>elemento3){
-        document.getElementById("1").innerHTML="La nota 1 es la mas alta "+elemento1;
+        document.getElementById("MayorNota").innerHTML="La nota 1 es la mas alta "+elemento1;
+        document.getElementById("MayorNota").style.color="blue";
     }
     else if(elemento2>elemento1 && elemento2>elemento3) {
-        document.getElementById("2").innerHTML="La nota 2 es la mas alta "+elemento2;
+        document.getElementById("MayorNota").innerHTML="La nota 2 es la mas alta "+elemento2;
+        document.getElementById("MayorNota").style.color="blue";
     } 
     else if(elemento3>elemento1 && elemento3>elemento2) {
-    document.getElementById("3").innerHTML="La nota 3 es la mas alta "+elemento3;
-    }
+    document.getElementById("MayorNota").innerHTML="La nota 3 es la mas alta "+elemento3;
+    document.getElementById("MayorNota").style.color="blue";
+   
+}
     else if(elemento3==elemento1 && elemento3>elemento2) {
-    document.getElementById("3").innerHTML="La nota 3 es la mas alta "+elemento3;
-    document.getElementById("1").innerHTML="La nota 1 es la mas alta "+elemento1;
+    document.getElementById("MayorNota").innerHTML="La nota 3 es la mas alta "+elemento3+" La nota 1 es la mas alta "+elemento1;
+    document.getElementById("MayorNota").style.color="blue";
     }
     else if(elemento2==elemento3 && elemento2>elemento1) {
-    document.getElementById("2").innerHTML="La nota 2 es la mas alta "+elemento2;
-    document.getElementById("3").innerHTML="La nota 3 es la mas alta "+elemento3;
+    document.getElementById("MayorNota").innerHTML="La nota 2 es la mas alta "+elemento2+" La nota 3 es la mas alta "+elemento3;
+    document.getElementById("MayorNota").style.color="blue";
     }
     else{
-    document.getElementById("1").innerHTML="La nota 1 es la mas alta "+elemento1;
-    document.getElementById("2").innerHTML="La nota 2 es la mas alta "+elemento2;
+    document.getElementById("MayorNota").innerHTML="La nota 1 es la mas alta "+elemento1+" La nota 2 es la mas alta "+elemento2;
+    document.getElementById("MayorNota").style.color="blue";
     }
     }
 
